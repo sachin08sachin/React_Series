9d4737d57255dc3552e418722b85dbfe55a12b06
sachin08sachin <sgsachin359@gmail.com>
2024-02-18 19:13:55 +0530

    commit

---
+++ Inception/app.js
@@ -0,0 +1,17 @@
+// const heading = React.createElement("h1",{id:"heading", xyz:"abc" }, "Hello React!");
+// console.log(heading);
+//     const root=ReactDOM.createRoot(document.getElementById("root"));
+//     root.render(heading);
+const parent = React.createElement("div",{id:"parent"},
+[ React.createElement("div",{id:"child1"},
+   [
+   React.createElement("h1",{},"I am h1 tag"),
+   React.createElement("h2",{},"i am h2 tag ")
+   ] ),
+   React.createElement("div",{id:"child2"},
+   [
+   React.createElement("h1",{},"I am h1 tag"),
+  React.createElement("h2",{},"i am h2 tag ")  ] )
+]);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
